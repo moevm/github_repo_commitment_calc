7b4a08079e0ac67da549254566c505bbecb57560
Konstantin Evgenievich Kondratenko <90711883+KonstantinKondratenko@users.noreply.github.com>
2025-06-25 12:53:45 +0300

    the second fix linter in utils.py

--- src/utils.py
+++ src/utils.py
@@ -116,4 +116,4 @@ def validate_and_normalize_cell(cell: str):
         raise ValueError(f"Cell contains non-latin characters: '{letters}'. Only Latin letters allowed.")
 
     return letters.upper() + numbers
-    
+
